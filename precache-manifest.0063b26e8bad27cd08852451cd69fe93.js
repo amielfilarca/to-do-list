14246a37a4c0039c685e69a8de6a7306c676fc62
Amiel Filarca <amielfilarca@gmail.com>
2020-09-28 16:59:23 +0800

    Updates

---
+++ precache-manifest.0063b26e8bad27cd08852451cd69fe93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c622f7da4bd5cb4beb4c7fa515dd25c",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "f9d098e7e4a542b4c42f",
+    "url": "/to-do-list/static/css/main.0cfd3d53.chunk.css"
+  },
+  {
+    "revision": "a0c4bd4e235016595579",
+    "url": "/to-do-list/static/js/2.a9a4c6b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-list/static/js/2.a9a4c6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d098e7e4a542b4c42f",
+    "url": "/to-do-list/static/js/main.7854ae97.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
